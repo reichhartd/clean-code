c4cc2828aca70137f093c36a49820ffb991e4208
Tobias Wittekindt <tobi@ottisoftware.com>
2020-09-07 14:42:45 +0200

    our testing script can now run actual tests.

--- testing.py
+++ testing.py
@@ -1,7 +1,17 @@
 # This is just for testing
+
 class Testing:
 
+    def __init__(self):
+        self.__run_tests()
+
     @staticmethod
-    def assert_equal(a, b):
-        if a != b:
-            raise Exception('Test Failed!', str(a) + ' != ' + str(b))
+    def assert_equal(expect, actual):
+        if expect != actual:
+            raise Exception('Test Failed!', str(expect) + ' != ' + str(actual))
+
+    def __run_tests(self):
+        # Run
+
+
+Testing()
