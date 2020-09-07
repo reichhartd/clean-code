0fb7f5fcbd856dfa07720455b42f79835aa1f275
Tobias Wittekindt <tobi@ottisoftware.com>
2020-09-07 14:51:29 +0200

    tests: Added our first test, which will check that a new order will be created correctly using the create_new_order_and_print function

--- testing.py
+++ testing.py
@@ -1,4 +1,6 @@
 # This is just for testing
+from LU import badSmell
+from LU.badSmell import orderItem
 
 class Testing:
 
@@ -11,7 +13,19 @@ class Testing:
             raise Exception('Test Failed!', str(expect) + ' != ' + str(actual))
 
     def __run_tests(self):
-        # Run
+        self.__test_create_order()
+
+    def __test_create_order(self):
+        order_item = orderItem('Test Item')
+        order_item.price = 1
+        order = badSmell.create_new_order_and_print('Test Order', [order_item], 'ABC', 1.16)
+        self.assert_equal(order['name'], 'Test Order')
+        self.assert_equal(len(order['items']), 1)
+        self.assert_equal(order['items'][0].name, 'Test Item')
+        self.assert_equal(order['items'][0].price, 1)
+        self.assert_equal(order['items'][0].priceWithTax, 1.16)
+
+
 
 
 Testing()
