5aa82423e1a2c853f1f17012e2cb9fd19fb301f4
Tobias Wittekindt <t.wittekind1@gmail.com>
2020-09-17 12:57:52 +0200

    feature: new function that splits up the complete file path into the file name and the parent folder of the file

--- FilePath.py
+++ FilePath.py
@@ -1,3 +1,11 @@
+def __split_path(file_path):
+    try:
+        last_slash_index = file_path.rindex('/') + 1
+        file_path_part = file_path[:last_slash_index]
+        filename = file_path[last_slash_index:]
+        return file_path_part, filename
+    except:
+        return '', file_path
 
 def get_path_part(filename):
     try:
@@ -31,6 +39,8 @@ def get_file_type(filename):
         return ''
 
 
+assert(__split_path("log/cups/access_log")[0] == "log/cups/")
+assert(__split_path("log/cups/access_log")[1] == "access_log")
 
 assert(get_path_part("log/cups/access_log") == "log/cups/")
 assert(get_path_part("log/cups/") == "log/cups/")
