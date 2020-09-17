dd3f03fd3863cdf3cd45acd611eacf512584df73
Tobias Wittekindt <t.wittekind1@gmail.com>
2020-09-17 13:19:37 +0200

    refactor: move get_file_name_part function to FilePath class

--- FilePath.py
+++ FilePath.py
@@ -13,10 +13,10 @@ class FilePath:
     def get_path_part(self, file_path):
         return self.split_path(file_path)[0]
 
+    def get_filename_part(self, file_path):
+        return self.split_path(file_path)[1]
 
 
-def get_filename_part(file_path):
-    return FilePath.split_path(file_path)[1]
 
 
 def get_file_type(file_path):
@@ -34,10 +34,10 @@ assert(FilePath().get_path_part("log/cups/access_log") == "log/cups/")
 assert(FilePath().get_path_part("log/cups/") == "log/cups/")
 assert(FilePath().get_path_part("cups/access_log") == "cups/")
 assert(FilePath().get_path_part("access_log") == "")
-assert(get_filename_part("log/cups/access_log") == "access_log")
-assert(get_filename_part("log/cups/") == "")
-assert(get_filename_part("cups/access_log") == "access_log")
-assert(get_filename_part("access_log") == "access_log")
+assert(FilePath().get_filename_part("log/cups/access_log") == "access_log")
+assert(FilePath().get_filename_part("log/cups/") == "")
+assert(FilePath().get_filename_part("cups/access_log") == "access_log")
+assert(FilePath().get_filename_part("access_log") == "access_log")
 assert(get_file_type("log/cups/access_log") == "")
 assert(get_file_type("base/FileHelper.cpp") == "cpp")
 assert(get_file_type("base/FileHelper.cpp.bak") == "bak")
